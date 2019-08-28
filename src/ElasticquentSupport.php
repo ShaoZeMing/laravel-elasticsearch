bf871b69545463ba1ebf6ce83b2ce48c9a779f82
shaozeming <szm19920426@gmail.com>
2019-08-28 21:42:28 +0800

    bug

--- src/ElasticquentSupport.php
+++ src/ElasticquentSupport.php
@@ -2,16 +2,10 @@
 
 namespace ShaoZeMing\LaravelElasticsearch;
 
-//use Illuminate\Foundation\Application;
-use Laravel\Lumen\Application;
 
 class ElasticquentSupport
 {
     use ElasticquentClientTrait;
 
-    public static function isLaravel5()
-    {
-        return version_compare(Application::VERSION, '5', '>');
-    }
 
 }
